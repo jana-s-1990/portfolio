3cfca76083672f145bed75515ec6041ed794731b
Jana S. <schaaf.jana@outlook.de>
2026-07-20 13:48:16 +0200

    add contact form

--- src/app/components/contact-form/contact-form.ts
+++ src/app/components/contact-form/contact-form.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
 import { Button } from '../button/button';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { email } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-contact-form',
-  imports: [Button],
+  imports: [Button, ReactiveFormsModule],
   templateUrl: './contact-form.html',
   styleUrl: './contact-form.scss',
 })
-export class ContactForm {}
+export class ContactForm {
+  contactForm = new FormGroup({
+    name: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.minLength(2)],
+    }),
+    email: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.email],
+    }),
+    message: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.minLength(10)],
+    }),
+
+    privacyAccepted: new FormControl(false, {
+      nonNullable: true,
+      validators: [Validators.requiredTrue],
+    }),
+  });
+
+  onSubmit(): void {
+    if (this.contactForm.invalid) {
+      this.contactForm.markAllAsTouched();
+      return;
+    }
+    const formData = this.contactForm.getRawValue();
+    console.log(formData);
+  }
+}
--- src/app/features/contact/contact.ts
+++ src/app/features/contact/contact.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Headline } from '../../components/headline/headline';
 import { ContactForm } from '../../components/contact-form/contact-form';
+import { InfoText } from '../../components/info-text/info-text';
 
 @Component({
   selector: 'app-contact',
-  imports: [Headline, ContactForm],
+  imports: [Headline, ContactForm, InfoText],
   templateUrl: './contact.html',
   styleUrl: './contact.scss',
 })
